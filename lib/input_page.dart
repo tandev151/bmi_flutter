c1023c6539303d3ab2bd36c76dc0c05ccb7f7796
Tan IT <tandev151>
2024-03-15 07:23:41 +0700

    check final and const

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,70 +16,72 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ReusableCard(
-                      color: Color(0x000000),
-                    ),
-                    ReusableCard(
-                      color: Color(0x000000),
-                    ),
-                  ],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: Colors.blue,
+                  ),
                 ),
-              ),
-
-              //   //
-              ReusableCard(
-                color: Color(0x000000),
-              ),
-
-              Expanded(
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ReusableCard(
-                      color: Colors.blue,
-                    ),
-                    ReusableCard(
-                      color: Color(0x000000),
-                    ),
-                  ],
+                Expanded(
+                  child: ReusableCard(
+                    color: Colors.blue,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+
+          //
+          Expanded(
+            child: ReusableCard(
+              color: Color(0xFF1D1E77),
+            ),
+          ),
+
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: Colors.blue,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({this.color});
-  Color? color = Color(0x00000000);
+  const ReusableCard({required this.color});
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: color ?? Color(0xFF1DE33),
-        ),
-        height: 200,
-        width: 170,
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: color,
       ),
+      height: 200.0,
+      width: double.infinity,
     );
   }
 }
